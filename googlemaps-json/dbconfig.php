88eb87ba6f6de883ac0cea562ec295f82da37c98
Gamze Gülyol <ggulyol@gmail.com>
2018-03-30 12:25:09 +0300

    google-map-api-spatial-type

---
+++ googlemaps-json/dbconfig.php
@@ -0,0 +1,11 @@
+<?php
+      
+$server     = 'localhost';
+$username   = 'root';
+$password   = 'gamzegulyol';
+$database   = 'eczane';
+  
+$dsn        = "mysql:host=$server;dbname=$database";
+
+
+?>
